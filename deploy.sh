861972d5c640bd47e48a49c89cd616489758be61
Jakub Gadek <jgadek@sysmakers.com>
2019-12-05 11:35:46 +0100

    Deleted dist publish

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 ng build ngx-sys-common
 cd dist/ngx-sys-common && npm pack
-npm run build_lib && npm run npm_pack
-
 
 cd ../../dist_publish/ngx-sys-common
 cp -R ../../dist/ngx-sys-common/ ./
+
+git add .
+git commit -m $1
+git push origin master
